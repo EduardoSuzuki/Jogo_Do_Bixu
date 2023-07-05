ab17adf99430c9e9eb81b9b251f1905056e69ebe
Eduardo Suzuki Barbosa <72422481+EduardoSuzuki@users.noreply.github.com>
2023-07-05 11:33:44 -0300

    Atualização v.1.0.1

--- Bixo.c
+++ Bixo.c
@@ -26,16 +26,11 @@ setlocale(LC_ALL, "Portuguese");
     int fx52;
 
 //Variaveis que armazenam os resultados em Strings ao inves de numeros
-    char sfx11;
-    char sfx12;
-    char sfx21;
-    char sfx22;
-    char sfx31; 
-    char sfx32; 
-    char sfx41;
-    char sfx42;
-    char sfx51;
-    char sfx52;
+    char sfx1 [4];
+    char sfx2 [4];
+    char sfx3 [4];
+    char sfx4 [4];
+    char sfx5 [4];
 
 //Array para armazenar os valores dos bixos
     int avestruz[4] = {1, 2, 3 ,4};
@@ -73,6 +68,12 @@ setlocale(LC_ALL, "Portuguese");
 //Variavél para receber valores apostados
     int aposta1;
     int aposta2;
+
+//Variável para receber aposta na Milhar
+    int numeros_aposta_milhar;
+
+//Varivél para recber aposta no bixo
+    int bixo_escolha;
 
 //Variavél para armazenar o valor a ser apostado
     int valor_aposta;
@@ -112,36 +113,56 @@ void menu() {
     printf("\nEscolha o que deseja fazer: ");
 }
 
+//Função para subtrair valor apostado da carteira
+void subtrai_carteira() {
+    valor_carteira = valor_carteira - valor_aposta;
+}
+
  //Função para receber duas dezenas 
 void recebe_valores_aposta_dezena() {
     printf("\nEscolha duas dezenas que deseja apostar.");
-    while(aposta1 > 99 || aposta1 < 0) {
+    do{
     printf("\nEscolha a primeira dezena: ");
     scanf("%d", &aposta1);
     if(aposta1 > 99 || aposta1 < 0) {
         printf("\nDezena inválida, digite uma dezena entre 0 e 99!");
         }
-    }
-    while(aposta2 > 99 || aposta2 < 0) {
+    } while(aposta1 > 99 || aposta1 < 0);
+    do{
     printf("\nEscolha a segunda dezena: ");
     scanf("%d", &aposta2);
     if(aposta2 > 99 || aposta2 < 0) {
         printf("\nDezena inválida, digite uma dezena entre 0 e 99!");
         }
-    }
+    } while(aposta2 > 99 || aposta2 < 0);
     printf("Dezenas escolhidas: %d e %d.\n", aposta1, aposta2);
+}
+
+//Função para receber valor aposta milhar
+void recebe_valores_aposta_milhar() {
+    printf("\nVocê escolheu a aposta na milhar!");
+     while(numeros_aposta_milhar > 9998 || numeros_aposta_milhar < 0) {
+    printf("\nDigite a milhar de deseja apostar: ");
+    scanf("%d", &numeros_aposta_milhar);
+        if(numeros_aposta_milhar > 9998 || numeros_aposta_milhar < 0001) {
+            printf("\nMilhar inválida, digite um numero entre 0001 e 9998!");
+        }
+    }
+    printf("\nNúmero escolhido: %d", numeros_aposta_milhar);
 }
 
 //Função de calcular premiação na primeira faixa
 void calculo_aposta_cabeca() {
     valor_premio_primeira_faixa = valor_aposta * 60;
     valor_carteira = valor_premio_primeira_faixa;
+    subtrai_carteira();
 }
 
 //Função de calcular premiação nas demais faixas
 void calculo_aposta_dezena() {
     valor_premio_demais_faixas = valor_aposta * 12;
     valor_carteira = valor_premio_demais_faixas;
+    subtrai_carteira();
 }
 
 //Função para analisar se ganhou na primeira faixa
@@ -166,6 +187,16 @@ void analise_aposta_dezena() {
         printf("\nVocê ganhou %d reais nas demais faixas!\n", valor_premio_demais_faixas);
     } else {
         printf("\nVocê não ganhou da segunda a quarta faixa!\n");
+    }
+}
+
+//Função para analisar se ganhou na milhar
+void analise_aposta_milhar() {
+    if(numeros_aposta_milhar = sfx1) {
+        printf("\nVocê ganhou na milhar!");
+        
+    } else {
+        printf("\nVocê não ganhou na milhar!");
     }
 }
 
@@ -237,20 +268,124 @@ void aposta_dezena() {
             } while(valor_aposta <= 0);
 }
 
+//Função aposta na Milhar
+void aposta_milhar() {
+    do{
+        printf("\nDigite o valor que deseja apostar: ");
+        scanf("%d", &valor_aposta);
+            if (valor_aposta <= 0) {
+                printf("Valor de aposta não pode ser 0 ou números negativos!!\n");
+            } else if(valor_aposta <= valor_carteira) {
+                printf("Você apostou %d reais!\n", valor_aposta);
+                recebe_valores_aposta_milhar();
+                analise_aposta_milhar();
+                resultados();
+            } else {
+                printf("Valor de aposta desejado é maior que o valor da carteira. \n");
+                printf("Tente novamente com outro valor ou deposite mais dinheiro em sua carteira.");
+            }
+    } while(valor_aposta <= 0);
+}
+
 //Função aposta no Bixo
 void aposta_no_bixo() {
-    printf("Capibara");
+    printf("1. AVESTRUZ\n2. ÁGUIA\n3. BURRO\n4. BORBOLETA\n5. CACHORRO\n6. CABRA\n7. CARNEIRO\n8. CAMELO\n9. COBRA\n10. COELHO\n11. CAVALO\n12. ELEFANTE\n13. GALO\n14. GATO\n15. JACARÉ\n16. LEÃO\n17. MACACO\n18. PORCO\n19. PAVÃO\n20. PERU\n21. TOURO\n22. TIGRE\n23. URSO\n24. VEADO\n25. VACA\n");
+    printf("Escolha o bixo: ");
+    scanf("%d", &bixo_escolha);
+        switch (bixo_escolha) {
+            case 1:
+                printf("Bixo escolhido: Avestruz\n");
+                break;
+            case 2:
+                printf("Bixo escolhido: Aguia\n");
+                break;
+            case 3:
+                printf("Bixo escolhido: Burro\n");
+                break;
+            case 4:
+                printf("Bixo escolhido: Borboleta\n");
+                break;
+            case 5:
+                printf("Bixo escolhido: Cachorro\n");
+                break;
+            case 6:
+                printf("Bixo escolhido: Cabra\n");
+                break;
+            case 7:
+                printf("Bixo escolhido: Carneiro\n"); 
+                break;
+            case 8:
+                printf("Bixo escolhido: Camelo\n");
+                break;
+            case 9:
+                printf("Bixo escolhido: Cobra\n");
+                break;
+            case 10:
+                printf("Bixo escolhido: Coelho\n");
+                break;
+            case 11:
+                printf("Bixo escolhido: Cavalo\n");
+                break;
+            case 12:
+                printf("Bixo escolhido: Elefante\n");
+                break;
+            case 13:
+                printf("Bixo escolhido: Galo\n");
+                break;
+            case 14:
+                printf("Bixo escolhido: Gato\n"); 
+                break;       
+            case 15:
+                printf("Bixo escolhido: Jacaré\n");
+                break;
+            case 16:
+                printf("Bixo escolhido: Leão\n");
+                break;
+            case 17:
+                printf("Bixo escolhido: Macaco\n");
+                break;
+            case 18:
+                printf("Bixo escolhido: Porco\n");
+                break;
+            case 19:
+                printf("Bixo escolhido: Pavão\n");
+                break;
+            case 20:
+                printf("Bixo escolhido: Peru\n");
+                break;
+            case 21:
+                printf("Bixo escolhido: Touro\n");
+                break;
+            case 22:
+                printf("Bixo escolhido: Tigre\n"); 
+                break;
+            case 23:
+                printf("Bixo escolhido: Urso\n");
+                break;
+            case 24:
+                printf("Bixo escolhido: Veado\n");
+                break;
+            case 25:
+                printf("Bixo escolhido: Vaca\n");
+                break;
+        default:
+            printf("Opção inválida!\n");
+        }
 }
 
 //Função que converte os resultados em string
 void converter_resultados_em_string() {
-    sprintf();
+    sprintf(sfx1, "%d%d", fx11, fx12);   
+    sprintf(sfx2, "%d%d", fx21, fx22);
+    sprintf(sfx3, "%d%d", fx31, fx32);
+    sprintf(sfx4, "%d%d", fx41, fx42);
+    sprintf(sfx5, "%d%d", fx51, fx52);
 }
 
 //Função para escolher o tipo de aposta
 void aposta_escolha() {
     do{
-    printf("\n1) Dezena na Cabeça.\n2) Dezena.\n3) Centena. \n4) Milhar. \n5) Milhar.");
+    printf("\n1) Dezena na Cabeça.\n2) Dezena.\n3) Centena. \n4) Milhar. \n5) Bixo.");
     printf("\nEscolha que tipo de aposta deseja fazer: ");
     scanf("%d", &aposta_tipo);
         if(aposta_tipo == 1) {
@@ -266,10 +401,11 @@ void aposta_escolha() {
 
         } else if(aposta_tipo == 4){
             printf("\nAposta Escolhida: Milhar.");
+            aposta_milhar();
 
         } else if(aposta_tipo == 5){
-            printf("\nAposta Escolhida: Bixo.");
-
+            printf("\nAposta Escolhida: Bixo.\n");
+            aposta_no_bixo();
         } else {
             printf("\nAposta escolhida inválida, digite o número novamente.");
         }
@@ -301,8 +437,8 @@ do {
 }
 
 /*======================================================= Execução do código ========================================================*/
-
 define_numeros();
+converter_resultados_em_string();
 menu_principal();
 
     return 0;
